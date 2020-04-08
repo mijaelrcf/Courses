e9cbf57c076ac0b344d3481297ddbb66a62793a8
Mijael Callejas <mijael.rcf@gmail.com>
2020-04-07 20:15:39 -0400

    adding first course classes

---
+++ basic-programming-course/Canvas/dibujo.js
@@ -0,0 +1,27 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var yi, xf;
+var colorcito = "red";
+
+while (l < lineas) {
+  yi = 10 * l;
+  xf = 10 * (l + 1);
+  dibujarLinea(colorcito, 0, yi, xf, 300);
+  console.log("Linea" + l);
+  l = l + 1;
+}
+
+dibujarLinea(colorcito, 1, 1, 1, 300);
+dibujarLinea(colorcito, 1, 299, 299, 299);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+}
---
+++ basic-programming-course/dom/codigo.js
@@ -0,0 +1,6 @@
+
+document.write("Hola estudiante");
+document.write("\n\n");
+var ruta = navigator;
+console.log(ruta);
+document.write("Estás en: " + ruta);
