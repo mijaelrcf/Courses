25628aebdb6e061fcc9dc336c72ca529286a385a
Mijael Callejas <mijael.rcf@gmail.com>
2020-04-08 21:03:55 -0400

    add flask wtf

--- python-and-django-career/flask-python-course/platzi-flask/main.py
+++ python-and-django-career/flask-python-course/platzi-flask/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -9,6 +12,10 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
 
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de Usuario', validators=[DataRequired])
+    password = PasswordField('Password', validators=[DataRequired])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -34,9 +41,11 @@ def index():
 def hello():
     # user_ip = request.cookies.get('user_ip') # with coockies
     user_ip = session.get('user_ip') # with sessions
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form,
     }
     
     return render_template('hello.html', **context) 
--- python-and-django-career/flask-python-course/platzi-flask/readme.txt
+++ python-and-django-career/flask-python-course/platzi-flask/readme.txt
@@ -1,14 +1,14 @@
-Install python3
-Install pip
+# Install python3
+# Install pip
 
-Install virtualenv
+# Install virtualenv
 virtualenv venv 
 venv\Scripts\activate
 
+# option 1
 pip install flask
-
-flask run
-
+# option 2
+pip install -r requirements.txt
 
 # SET VARIABLE TO RUN FLASK
 # mac and linux
@@ -18,6 +18,9 @@ export FLASK_APP=main.py
 # set FLASK_APP=main.py
 $env:FLASK_APP = "main.py"
 
+# run flask
+flask run
+
 # Debugging en Flask
 $env:FLASK_DEBUG = 0 #do not show errors
 $env:FLASK_DEBUG = 1 #show errors
@@ -25,3 +28,6 @@ $env:FLASK_DEBUG = 1 #show errors
 # change environment to development
 $env:FLASK_ENV='production'
 $env:FLASK_ENV='development' 
+
+# Install wtf
+pip install -r requirements.txt
