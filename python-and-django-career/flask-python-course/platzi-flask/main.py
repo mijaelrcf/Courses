7c55aaeab34b26c87aa2711f994134c1745a8bc0
Mijael Callejas <mijael.rcf@gmail.com>
2020-04-08 22:16:26 -0400

    use methods get and post, add validators and validate_on_submit

--- python-and-django-career/flask-python-course/platzi-flask/main.py
+++ python-and-django-career/flask-python-course/platzi-flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
@@ -13,9 +13,10 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor']
 
 class LoginForm(FlaskForm):
-    username = StringField('Nombre de Usuario', validators=[DataRequired])
-    password = PasswordField('Password', validators=[DataRequired])
+    username = StringField('Nombre de Usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -37,16 +38,25 @@ def index():
 
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     # user_ip = request.cookies.get('user_ip') # with coockies
     user_ip = session.get('user_ip') # with sessions
     login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
         'login_form': login_form,
+        'username': username
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        return redirect(url_for('index'))
     
     return render_template('hello.html', **context) 
 
