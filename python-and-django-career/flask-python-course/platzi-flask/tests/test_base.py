74bb4675d9efcf7bdcb3ac3508ddbbe1f9aedbf5
Mijael Callejas <mijael.rcf@gmail.com>
2020-04-10 03:36:14 -0400

    move login_form from hello to views.py

--- python-and-django-career/flask-python-course/platzi-flask/tests/test_base.py
+++ python-and-django-career/flask-python-course/platzi-flask/tests/test_base.py
@@ -32,13 +32,13 @@ class MainTest(TestCase):
 
     
     def test_hello_post(self):
-        fake_form = {
-            'username': 'fake',
-            'password': 'fake-password'
-        }
-        response = self.client.post(url_for('hello'), data=fake_form)
+        # fake_form = {
+        #     'username': 'fake',
+        #     'password': 'fake-password'
+        # }
+        response = self.client.post(url_for('hello'))
 
-        self.assertRedirects(response, url_for('index'))
+        self.assertTrue(response.status_code, 405)
 
     def test_auth_blueprint_exists(self):
         self.assertIn('auth', self.app.blueprints)
@@ -53,3 +53,13 @@ class MainTest(TestCase):
         self.client.get(url_for('auth.login'))
 
         self.assertTemplateUsed('login.html')
+
+    
+    def test_auth_login_post(self):
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake-password'
+        }
+
+        response = self.client.post(url_for('auth.login'), data=fake_form)
+        self.assertRedirects(response, url_for('index'))
