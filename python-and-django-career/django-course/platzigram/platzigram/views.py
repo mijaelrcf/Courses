5f4814a851d7a05fbc349db8b1c63c564eb8ec46
Mijael Callejas <mijael.rcf@gmail.com>
2020-04-13 23:27:21 -0400

    add method sorted integers and say hello with return json

--- python-and-django-career/django-course/platzigram/platzigram/views.py
+++ python-and-django-career/django-course/platzigram/platzigram/views.py
@@ -1,10 +1,12 @@
 """Platzigram URLs module."""
 # Django
 from django.http import HttpResponse
-from django.http import JsonResponse
+#from django.http import JsonResponse
 
 # Utilities
 from datetime import datetime
+import json
+
 
 def hello_world(request):
     """return a greeting."""    
@@ -13,9 +15,31 @@ def hello_world(request):
         now=datetime.now().strftime('%b %dth, %Y - %H:%M hrs')
     ))
 
-def hi(request):
-    """Hi."""
+def sort_integers(request):
+    """return a JSON response with sorted integers."""
     numbers = request.GET['numbers']
-    sorted_number = sorted([int(x) for x in numbers.split(',')])
     #import pdb; pdb.set_trace()
-    return HttpResponse(JsonResponse({'sorted_number': sorted_number}))
+    sorted_numbers = sorted([int(i) for i in numbers.split(',')])
+
+    data = {
+        'status': 'ok',
+        'numbers': sorted_numbers,
+        'message': 'Integers sorted successfully'
+    }
+    
+    return HttpResponse(
+        json.dumps(data, indent=4), 
+        content_type='application/json'
+    )
+    #return HttpResponse(str(sorted_numbers), content_type='application/json')
+    #return HttpResponse(JsonResponse({'sorted_number': sorted_numbers}))
+
+
+def say_hi(request, name, age):
+    """return a greeting."""
+    if age < 12:
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}: Welcome to Platzigram'.format(name)
+
+    return HttpResponse(message)
