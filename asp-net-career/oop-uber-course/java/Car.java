9fd7eca94f879ae64216c4aebdf14f3de18d1ffe
Mijael Callejas <mijael.rcf@gmail.com>
2020-05-10 23:32:20 -0400

    add inheritance and polymorphism

--- asp-net-career/oop-uber-course/java/Car.java
+++ asp-net-career/oop-uber-course/java/Car.java
@@ -12,4 +12,42 @@ public class Car {
     void printDataCar(){
         System.out.println("License: " + license + "Name Driver: " + driver.name);
     }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLicense() {
+		return license;
+	}
+
+	public void setLicense(String license) {
+		this.license = license;
+	}
+
+	public Account getDriver() {
+		return driver;
+	}
+
+	public void setDriver(Account driver) {
+		this.driver = driver;
+	}
+
+	public Integer getPassenger() {
+		return passenger;
+	}
+
+	public void setPassenger(Integer passenger) {
+        if (passenger == 4) {
+            this.passenger = passenger;    
+        }
+        else {
+            System.out.println("Debe ingresar 4 pasajeros");
+        }
+        
+	}
 }
