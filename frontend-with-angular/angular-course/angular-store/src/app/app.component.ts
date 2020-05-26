0123f6152c4deb319d348bb6fd70c75b5dd43f2e
Mijael Callejas <mijael.rcf@gmail.com>
2020-05-26 15:35:26 -0400

    use of ngFor and creation of methods in app.component.ts

--- frontend-with-angular/angular-course/angular-store/src/app/app.component.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-store';
+
+  items = ['mijael', 'rodrigo', 'callejas'];
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
 }
