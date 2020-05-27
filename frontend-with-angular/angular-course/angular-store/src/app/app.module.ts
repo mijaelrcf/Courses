503a96d1c7780c9ff8eed1e7f9041fe4c08d01bb
Mijael Callejas <mijael.rcf@gmail.com>
2020-05-26 20:20:17 -0400

    use of components and decorators

--- frontend-with-angular/angular-course/angular-store/src/app/app.module.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/app.module.ts
@@ -1,18 +1,24 @@
+// Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// Local
 import { FormsModule } from '@angular/forms';
 
+// Angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// Local
+import { ProductComponent } from './components/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent // Local
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule // Local
   ],
   providers: [],
   bootstrap: [AppComponent]
