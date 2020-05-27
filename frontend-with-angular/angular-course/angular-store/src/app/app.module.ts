734b1f809b76257d8ee57f36d7231b3335826593
Mijael Callejas <mijael.rcf@gmail.com>
2020-05-27 18:49:13 -0400

    creation of an own directive

--- frontend-with-angular/angular-course/angular-store/src/app/app.module.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/app.module.ts
@@ -11,13 +11,15 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
 import { CartComponent } from './cart/cart.component';
 import { ExponentialPipe } from './exponential.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
+    ProductComponent, // Local
     CartComponent,
-    ExponentialPipe // Local
+    ExponentialPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
