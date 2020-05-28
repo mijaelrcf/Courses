61ed036667b01ecd006ff5bb025ed5f52d8bbb57
Mijael Callejas <mijael.rcf@gmail.com>
2020-05-27 20:16:18 -0400

    createion routes and components: home, contact, products and demo

---
+++ frontend-with-angular/angular-course/angular-store/src/app/demo/demo.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss']
+})
+
+export class DemoComponent implements OnInit {
+  titlePage = 'angular-store';
+  textTest = '';
+  name = '';
+
+  items = ['mijael', 'rodrigo', 'callejas'];
+
+  power = 10;
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
