cae05972158e75fde6bd3a2c260a013ef97feaf0
Mijael Callejas <mijael.rcf@gmail.com>
2020-05-28 22:16:00 -0400

    using shared module and core module

--- frontend-with-angular/angular-course/angular-store/src/app/app-routing.module.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
       },
       {
         path: 'home',
-        loadChildren: () => import('./home/home.module').then(m=> m.HomeModule)
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
       },
       {
         path: 'products',
--- frontend-with-angular/angular-course/angular-store/src/app/home/home.module.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from "@angular/common";
+import { CommonModule } from '@angular/common';
 
 import { BannerComponent } from './components/banner/banner.component';
 import { HomeComponent } from './components/home/home.component';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import { HomeRoutingModule } from './home-routing.module';
     ],
     imports: [
         CommonModule,
+        SharedModule,
         HomeRoutingModule
     ]
 })
