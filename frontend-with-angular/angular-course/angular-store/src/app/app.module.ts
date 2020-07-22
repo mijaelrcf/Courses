8c601edc61591cd350c4d6cdbddf2c6eb0411991
Mijael Callejas <mijael.rcf@gmail.com>
2020-07-22 19:50:56 -0400

    se modularizaron todos los componentes

--- frontend-with-angular/angular-course/angular-store/src/app/app.module.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/app.module.ts
@@ -7,30 +7,18 @@ import { FormsModule } from '@angular/forms';
 // Angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 // Local
-import { ProductComponent } from './product/product.component';
-import { CartComponent } from './cart/cart.component';
-
-import { ProductsComponent } from './products/products.component';
-import { ContactComponent } from './contact/contact.component';
-import { DemoComponent } from './demo/demo.component';
-
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+//import { CartComponent } from './cart/cart.component';
 import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule  } from './core/core.module';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent, // Local
-    CartComponent,
-    ProductsComponent,
-    ContactComponent,
-    DemoComponent,
-    PageNotFoundComponent,
-    ProductDetailComponent,
+    //CartComponent,
     LayoutComponent
   ],
   imports: [
