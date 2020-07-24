5eb04e31e106551a742be5fd714e25af8c8d92bb
Mijael Callejas <mijael.rcf@gmail.com>
2020-07-24 19:41:32 -0400

    spaces removed

--- frontend-with-angular/angular-course/angular-store/src/app/admin/admin-routing.module.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/admin/admin-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
       {
         path: 'create',
         component: ProductFormComponent
-      },      
+      },
       {
         path: 'table',
         component: TableComponent
