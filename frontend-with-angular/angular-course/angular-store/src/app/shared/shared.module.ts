b483ac5bf0a2fe85db8200de3a5447db6c0245da
Mijael Callejas <mijael.rcf@gmail.com>
2020-07-24 12:22:13 -0400

    using mat-toolbar, mat-icon, mat-badge and flexboxgrid

--- frontend-with-angular/angular-course/angular-store/src/app/shared/shared.module.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/shared/shared.module.ts
@@ -8,6 +8,8 @@ import { HighlightDirective } from './directives/highlight/highlight.directive';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+import { MaterialModule } from './../material/material.module';
+
 @NgModule({
   declarations: [
     ExponentialPipe,
@@ -23,7 +25,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ]
 })
 export class SharedModule { }
