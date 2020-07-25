5fcd84e2afdf2739a00ee0227c8e24ac96241c57
Mijael Callejas <mijael.rcf@gmail.com>
2020-07-24 22:46:43 -0400

    implemented request post

--- frontend-with-angular/angular-course/angular-store/src/app/product/components/product-detail/product-detail.component.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/product/components/product-detail/product-detail.component.ts
@@ -32,4 +32,19 @@ export class ProductDetailComponent implements OnInit {
       this.product = product;
     });
   }
+
+  createProduct() {
+    const newProduct: Product = {
+      id: '222',
+      title: 'nuevo desde angular',
+      image: 'assets/images/banner-1.jpg',
+      price: 3000,
+      description: 'nuevo producto'
+    };
+    this.productService.createProduct(newProduct)
+    .subscribe(product => {
+      console.log(product);
+      //this.product = product;
+    });
+  }
 }
