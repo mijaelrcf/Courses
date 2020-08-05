f2b8f9e6dab6f6b87edca4c2e210a372cfa339c6
Mijael Callejas <mijael.rcf@gmail.com>
2020-08-05 19:55:03 -0400

    install and config angular-firebase, auth with email and creation login and register forms

--- frontend-with-angular/angular-course/angular-store/src/app/app.module.ts
+++ frontend-with-angular/angular-course/angular-store/src/app/app.module.ts
@@ -1,9 +1,11 @@
 // Angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
 
 // Local
 import { FormsModule } from '@angular/forms';
@@ -12,6 +14,8 @@ import { LayoutComponent } from './layout/layout.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule  } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { environment } from './../environments/environment';
 
 
 @NgModule({
@@ -26,7 +30,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SharedModule,
     CoreModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend-with-angular/angular-course/angular-store/src/environments/environment.stag.ts
+++ frontend-with-angular/angular-course/angular-store/src/environments/environment.stag.ts
@@ -1,4 +1,13 @@
 export const environment = {
   production: true,
-  url_api: 'https://platzi-store-stag.herokuapp.com'
+  url_api: 'https://platzi-store-stag.herokuapp.com',
+  firebase: {
+    apiKey: 'AIzaSyD34_g1LVAWJXB2G4l41IbYqH7i4RSFDlw',
+    authDomain: 'db-angular-store.firebaseapp.com',
+    databaseURL: 'https://db-angular-store.firebaseio.com',
+    projectId: 'db-angular-store',
+    storageBucket: 'db-angular-store.appspot.com',
+    messagingSenderId: '878733621803',
+    appId: '1:878733621803:web:459d8decd27d505421c86e'
+  }
 };
