f6354802541e819f0dc1aad657fd107fa3b36cd6
Mijael Callejas <mijael.rcf@gmail.com>
2020-10-05 23:14:09 -0400

    Creation structure, using of enumeratings and creation of parameters

---
+++ asp-net-career/csharp-fundamentals-net-core/Etapa1/Entidades/Escuela.cs
@@ -0,0 +1,37 @@
+namespace CoreEscuela.Entidades
+{
+    class Escuela
+    {
+        string nombre;
+
+        public string Nombre
+        {
+            get { return "Copia: " + nombre; }
+            set { nombre = value.ToUpper(); }
+        }
+
+        public int AnioDeCreacion { get; set; }
+
+        public string Pais { get; set; }
+
+        public string Ciudad { get; set; }
+
+        public TiposEscuela TipoEscuela { get; set; }
+
+        public Escuela(string nombre, int anio) => (Nombre, AnioDeCreacion) = (nombre, anio);
+        public Escuela(string nombre, int anio,
+                        TiposEscuela tipo,
+                        string pais = "", string ciudad = "")
+        {
+            (Nombre, AnioDeCreacion) = (nombre, anio);
+            TipoEscuela = tipo;
+            Pais = pais;
+            this.Ciudad = ciudad;
+        }
+
+        public override string ToString()
+        {
+            return $"Nombre: \"{Nombre}\", Tipo: {TipoEscuela} {System.Environment.NewLine} Pais: {Pais}, Ciudad: {Ciudad}";
+        }
+    }
+}
---
+++ asp-net-career/csharp-fundamentals-net-core/Etapa1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("Platzi Academy", 2012);
+            escuela.Pais = "Colombia";
+            escuela.Ciudad = "Bogota";
+            escuela.TipoEscuela = TiposEscuela.Primaria;
+            
+            Console.WriteLine(escuela);
+            
+            var escuela2 = new Escuela("Platzi Academy", 2012, TiposEscuela.PreEscolar,
+                pais:"Bolivia"
+            );
+            Console.WriteLine(escuela2);
+        }
+    }
+}
