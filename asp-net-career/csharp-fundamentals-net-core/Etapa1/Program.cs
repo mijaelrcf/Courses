21973a74adc1f9a06800dfc61c75698fb9039419
Mijael Callejas <mijael.rcf@gmail.com>
2020-10-06 20:33:25 -0400

    maneras de recorrer un arreglo

--- asp-net-career/csharp-fundamentals-net-core/Etapa1/Program.cs
+++ asp-net-career/csharp-fundamentals-net-core/Etapa1/Program.cs
@@ -11,40 +11,43 @@ namespace Etapa1
             escuela.Pais = "Colombia";
             escuela.Ciudad = "Bogota";
             escuela.TipoEscuela = TiposEscuela.Primaria;
-            
+
             Console.WriteLine(escuela);
-            
+
             var escuela2 = new Escuela("Platzi Academy", 2012, TiposEscuela.PreEscolar,
-                pais:"Bolivia"
+                pais: "Bolivia"
             );
             Console.WriteLine(escuela2);
 
-            var curso3 = new Curso() {
+            var curso3 = new Curso()
+            {
                 Nombre = "101"
             };
-            
-            var curso4 = new Curso() {
+
+            var curso4 = new Curso()
+            {
                 Nombre = "201"
             };
 
-            var curso5 = new Curso() {
-                Nombre = "301"                
+            var curso5 = new Curso()
+            {
+                Nombre = "301"
             };
-            
+
             System.Console.WriteLine("===========");
             System.Console.WriteLine(curso3.Nombre + ", " + curso3.UniqueId);
             System.Console.WriteLine($"{curso4.Nombre} , {curso4.UniqueId}");
             System.Console.WriteLine(curso5);
 
             var arregloCursos = new Curso[3];
-            arregloCursos[0] = new Curso()            
+            arregloCursos[0] = new Curso()
             {
                 Nombre = "101"
             };
 
             var curso2 = new Curso()
             {
-              Nombre = "201"  
+                Nombre = "201"
             };
             arregloCursos[1] = curso2;
 
@@ -55,16 +58,49 @@ namespace Etapa1
 
             System.Console.WriteLine(escuela);
             System.Console.WriteLine("==============");
-            ImprimirCursos(arregloCursos);
+            System.Console.WriteLine("While");
+            ImprimirCursosWhile(arregloCursos);
+            System.Console.WriteLine("DoWhile");
+            ImprimirCursosDoWhile(arregloCursos);
+            System.Console.WriteLine("For");
+            ImprimirCursosFor(arregloCursos);
+            System.Console.WriteLine("Foreach");
+            ImprimirCursosForEach(arregloCursos);
         }
 
-        private static void ImprimirCursos(Curso[] arregloCursos)
+        private static void ImprimirCursosWhile(Curso[] arregloCursos)
         {
             int contador = 0;
             while (contador < arregloCursos.Length)
             {
                 Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, Id {arregloCursos[contador].UniqueId}");
-                contador = contador++;
+                contador++;
+            }
+        }
+
+        private static void ImprimirCursosDoWhile(Curso[] arregloCursos)
+        {
+            int contador = 0;
+            do
+            {
+                Console.WriteLine($"Nombre {arregloCursos[contador].Nombre}, Id {arregloCursos[contador].UniqueId}");
+                contador++;
+            } while (contador < arregloCursos.Length);
+        }
+
+        private static void ImprimirCursosFor(Curso[] arregloCursos)
+        {
+            for (int i = 0; i < arregloCursos.Length; i++)
+            {
+                Console.WriteLine($"Nombre {arregloCursos[i].Nombre}, Id {arregloCursos[i].UniqueId}");
+            }
+        }
+
+        private static void ImprimirCursosForEach(Curso[] arregloCursos)
+        {
+            foreach (var curso in arregloCursos)
+            {
+                Console.WriteLine($"Nombre { curso.Nombre }, Id { curso.UniqueId }");
             }
         }
     }
